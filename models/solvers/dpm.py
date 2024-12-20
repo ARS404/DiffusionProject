2bd97a0a5b17f78e97c32a7778ea4336a976f1df
Arseniy <arsenator337@gmail.com>
2024-12-21 01:35:43 +0300

    fix

--- models/solvers/dpm.py
+++ models/solvers/dpm.py
@@ -1,12 +1,154 @@
+import torch
+
 from .base import BaseSolver
 
 
 class DPMSolver(BaseSolver):
-    def __init__(self):
+    def __init__(self, t_start, t_end, order, skip_type):
         super().__init__()
+        self.t_start = t_start
+        self.t_end = t_end
 
-    def __call__(self, net, *args, **kwds):
-        return super().__call__(net, *args, **kwds)
-    
+        self.order = order
+        self.skip_type = skip_type
+
+    def __call__(self, net, noise, num_steps, labels=None, device='cuda'):
+        timesteps = self.get_time_steps(t_T=self.t_start, t_0=self.t_end, N=num_steps, device=device)
+        # Init the initial values.
+        step = 0
+        t = timesteps[step]
+        t_prev_list = [t]
+        model_prev_list = [self.model_fn(x, t)]
+        if self.correcting_xt_fn is not None:
+            x = self.correcting_xt_fn(x, t, step)
+        # Init the first `order` values by lower order multistep DPM-Solver.
+        for step in range(1, self.order):
+            t = timesteps[step]
+            x = self.multistep_dpm_solver_update(x, model_prev_list, t_prev_list, t, step)
+            if self.correcting_xt_fn is not None:
+                x = self.correcting_xt_fn(x, t, step)
+            t_prev_list.append(t)
+            model_prev_list.append(self.model_fn(x, t))
+        # Compute the remaining values by `order`-th order multistep DPM-Solver.
+        for step in range(self.order, num_steps + 1):
+            t = timesteps[step]
+            # We only use lower order for steps < 10
+            step_order = self.order
+            x = self.multistep_dpm_solver_update(x, model_prev_list, t_prev_list, t, step_order)
+            if self.correcting_xt_fn is not None:
+                x = self.correcting_xt_fn(x, t, step)
+            for i in range(self.order - 1):
+                t_prev_list[i] = t_prev_list[i + 1]
+                model_prev_list[i] = model_prev_list[i + 1]
+            t_prev_list[-1] = t
+            # We do not need to evaluate the final model value.
+            if step < num_steps:
+                model_prev_list[-1] = self.model_fn(x, t)
+
+        return x
+
+    def multistep_dpm_solver_update(self, x, model_prev_list, t_prev_list, t, order):
+        if order == 1:
+            return self.dpm_solver_first_update(x, t_prev_list[-1], t, model_s=model_prev_list[-1])
+        elif order == 2:
+            return self.multistep_dpm_solver_second_update(x, model_prev_list, t_prev_list, t)
+        return self.multistep_dpm_solver_third_update(x, model_prev_list, t_prev_list, t)
+
+    def get_time_steps(self, t_T, t_0, N, device):
+        if self.skip_type == 'logSNR':
+            lambda_T = self.noise_schedule.marginal_lambda(torch.tensor(t_T).to(device))
+            lambda_0 = self.noise_schedule.marginal_lambda(torch.tensor(t_0).to(device))
+            logSNR_steps = torch.linspace(lambda_T.cpu().item(), lambda_0.cpu().item(), N + 1).to(device)
+            return self.noise_schedule.inverse_lambda(logSNR_steps)
+        elif self.skip_type == 'time_uniform':
+            return torch.linspace(t_T, t_0, N + 1).to(device)
+        elif self.skip_type == 'time_quadratic':
+            t_order = 2
+            t = torch.linspace(t_T**(1. / t_order), t_0**(1. / t_order), N + 1).pow(t_order).to(device)
+            return t
+
+    def dpm_solver_first_update(self, x, s, t, model_s=None, return_intermediate=False):
+        ns = self.noise_schedule
+        dims = x.dim()
+        lambda_s, lambda_t = ns.marginal_lambda(s), ns.marginal_lambda(t)
+        h = lambda_t - lambda_s
+        log_alpha_s, log_alpha_t = ns.marginal_log_mean_coeff(s), ns.marginal_log_mean_coeff(t)
+        sigma_s, sigma_t = ns.marginal_std(s), ns.marginal_std(t)
+        alpha_t = torch.exp(log_alpha_t)
+
+        if self.algorithm_type == "dpmsolver++":
+            phi_1 = torch.expm1(-h)
+            if model_s is None:
+                model_s = self.model_fn(x, s)
+            x_t = (
+                sigma_t / sigma_s * x
+                - alpha_t * phi_1 * model_s
+            )
+            if return_intermediate:
+                return x_t, {'model_s': model_s}
+            else:
+                return x_t
+        else:
+            phi_1 = torch.expm1(h)
+            if model_s is None:
+                model_s = self.model_fn(x, s)
+            x_t = (
+                torch.exp(log_alpha_t - log_alpha_s) * x
+                - (sigma_t * phi_1) * model_s
+            )
+            if return_intermediate:
+                return x_t, {'model_s': model_s}
+            else:
+                return x_t
+
+    def multistep_dpm_solver_second_update(self, x, model_prev_list, t_prev_list, t):
+        ns = self.noise_schedule
+        model_prev_1, model_prev_0 = model_prev_list[-2], model_prev_list[-1]
+        t_prev_1, t_prev_0 = t_prev_list[-2], t_prev_list[-1]
+        lambda_prev_1, lambda_prev_0, lambda_t = ns.marginal_lambda(t_prev_1), ns.marginal_lambda(t_prev_0), ns.marginal_lambda(t)
+        log_alpha_prev_0, log_alpha_t = ns.marginal_log_mean_coeff(t_prev_0), ns.marginal_log_mean_coeff(t)
+        sigma_prev_0, sigma_t = ns.marginal_std(t_prev_0), ns.marginal_std(t)
+        alpha_t = torch.exp(log_alpha_t)
+
+        h_0 = lambda_prev_0 - lambda_prev_1
+        h = lambda_t - lambda_prev_0
+        r0 = h_0 / h
+        D1_0 = (1. / r0) * (model_prev_0 - model_prev_1)
+        phi_1 = torch.expm1(-h)
+        x_t = (
+            (sigma_t / sigma_prev_0) * x
+            - (alpha_t * phi_1) * model_prev_0
+            - 0.5 * (alpha_t * phi_1) * D1_0
+        )
+        return x_t
+
+    def multistep_dpm_solver_third_update(self, x, model_prev_list, t_prev_list, t):
+        ns = self.noise_schedule
+        model_prev_2, model_prev_1, model_prev_0 = model_prev_list
+        t_prev_2, t_prev_1, t_prev_0 = t_prev_list
+        lambda_prev_2, lambda_prev_1, lambda_prev_0, lambda_t = ns.marginal_lambda(t_prev_2), ns.marginal_lambda(t_prev_1), ns.marginal_lambda(t_prev_0), ns.marginal_lambda(t)
+        log_alpha_prev_0, log_alpha_t = ns.marginal_log_mean_coeff(t_prev_0), ns.marginal_log_mean_coeff(t)
+        sigma_prev_0, sigma_t = ns.marginal_std(t_prev_0), ns.marginal_std(t)
+        alpha_t = torch.exp(log_alpha_t)
+
+        h_1 = lambda_prev_1 - lambda_prev_2
+        h_0 = lambda_prev_0 - lambda_prev_1
+        h = lambda_t - lambda_prev_0
+        r0, r1 = h_0 / h, h_1 / h
+        D1_0 = (1. / r0) * (model_prev_0 - model_prev_1)
+        D1_1 = (1. / r1) * (model_prev_1 - model_prev_2)
+        D1 = D1_0 + (r0 / (r0 + r1)) * (D1_0 - D1_1)
+        D2 = (1. / (r0 + r1)) * (D1_0 - D1_1)
+        phi_1 = torch.expm1(-h)
+        phi_2 = phi_1 / h + 1.
+        phi_3 = phi_2 / h - 0.5
+        x_t = (
+            (sigma_t / sigma_prev_0) * x
+            - (alpha_t * phi_1) * model_prev_0
+            + (alpha_t * phi_2) * D1
+            - (alpha_t * phi_3) * D2
+        )
+        return x_t
+
     def get_name(self):
-        return "DPM"
+        return f"DPM_ord{self.order}"
--- models/solvers/edm.py
+++ models/solvers/edm.py
@@ -11,8 +11,9 @@ class EDMSolver(BaseSolver):
         self.sigma_max = sigma_max
         self.rho = rho
 
-    def __call__(self, net, noise, num_steps, 
-                 class_labels=None, randn_like=torch.randn_like):
+    def __call__(self, net, noise, num_steps, device='cuda', 
+                 labels=None, randn_like=torch.randn_like,
+                 S_churn=80, S_min=0.05, S_max=1.0, S_noise=1.007):
         
         sigma_min = max(self.sigma_min, net.sigma_min)
         sigma_max = min(self.sigma_max, net.sigma_max)
@@ -24,22 +25,28 @@ class EDMSolver(BaseSolver):
         )
         x_next = noise.to(torch.float64) * t_steps[0]
 
-        for i, (t_cur, t_next) in enumerate(zip(t_steps[:-1], t_steps[1:])):
-            x_cur = x_next
-
-            t_hat = net.round_sigma(t_cur * t_cur)
-            x_hat = x_cur + (t_hat ** 2 - t_cur ** 2).sqrt() * randn_like(x_cur)
-
-            denoised = net(x_hat, t_hat, class_labels).to(torch.float64)
-            d_cur = (x_hat - denoised) / t_hat
-            x_next = x_hat + (t_next - t_hat) * d_cur
-
+        for i in range(len(t_steps) - 1):
+            t_cur = t_steps[i]
+            t_next = t_steps[i + 1]
+            
+            gamma = min(S_churn / num_steps, np.sqrt(2) - 1) if S_min <= t_cur <= S_max else 0
+            t_hat_cur = (t_cur + gamma * t_cur)
+            
+            t_hat_cur_net = t_hat_cur * torch.ones(x.shape[0], device=device)
+            t_next_net = t_next * torch.ones(x.shape[0], device=device)
+            delta_t = (t_hat_cur - t_next).abs()
+            
+            x_hat = x + (t_hat_cur ** 2 - t_cur ** 2).sqrt() * S_noise * torch.randn_like(x)   
+            v_hat = self.velocity_from_denoiser(x_hat, net, t_hat_cur_net, labels)
+            x_next = x_hat + delta_t * v_hat
+        
             if i < num_steps - 1:
-                denoised = net(x_next, t_next, class_labels).to(torch.float64)
-                d_prime = (x_next - denoised) / t_next
-                x_next = x_hat + (t_next - t_hat) * (0.5 * d_cur + 0.5 * d_prime)
+                v_next = self.velocity_from_denoiser(x_next, net, t_next_net, labels)
+                x_next = x_hat + delta_t * (0.5 * v_hat + 0.5 * v_next)
 
-        return x_next
+            x = x_next
+            
+        return x
 
 
     def get_name(self):
