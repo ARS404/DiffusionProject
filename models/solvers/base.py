6a29fe993b8f04ecaece8d9ea56e56cfcca69052
Arseniy <arsenator337@gmail.com>
2024-12-20 16:33:26 +0300

    fix

--- models/solvers/base.py
+++ models/solvers/base.py
@@ -1,6 +1,32 @@
+import torch
+
+from abc import abstractmethod
+
+
 class BaseSolver(object):
-    def __call__(self, *args, **kwds):
+    def __call__(self, net, *args, **kwds):
         raise NotImplementedError()
     
+    @abstractmethod
+    def normalize(x):
+        return x / x.abs().max(dim=0)[0][None, ...]
+    
+    @abstractmethod
+    def get_timesteps(sigma_min=0.02, sigma_max=80.0, num_steps=20, device='cuda', rho=7.0):
+        step_indices = torch.arange(num_steps, device=device)
+        t_steps = (sigma_max ** (1 / rho) + step_indices / (num_steps - 1) * (sigma_min ** (1 / rho) - sigma_max ** (1 / rho))) ** rho
+        t_steps = torch.cat([t_steps, torch.zeros_like(t_steps[:1])]) # t_N = 0
+        return t_steps
+    
+    @abstractmethod
+    def velocity_from_denoiser(x, net, sigma, class_labels=None, error_eps=1e-4, stochastic=False):
+        sigma = sigma[:, None, None, None] # [batch, 1, 1, 1]
+        v = (net(x, sigma, class_labels) - x) / (sigma + error_eps)
+
+        if stochastic:
+            v = v * 2
+
+        return v
+
     def get_name(self):
         return "I have no name :("
--- models/solvers/euler.py
+++ models/solvers/euler.py
@@ -1,11 +1,36 @@
+import torch
+
 from .base import BaseSolver
 
-class EulerSolver(BaseSolver):
-    def __init__(self):
-        super().__init__()
 
-    def __call__(self, *args, **kwds):
-        pass
+class EulerSolver(BaseSolver):
+    def __init__(self, net=None):
+        super().__init__()        
+
+    def __call__(
+            self, noise, net, labels=None, sigma_min=0.02, 
+            sigma_max=80.0, num_steps=20, device='cuda', 
+            rho=7.0, stochastic=False, vis_steps=5
+    ):
+        t_steps = self.get_timesteps(sigma_min, sigma_max, num_steps, device, rho) # t_steps[0] = 80.0, t_steps[-2] = 0.02, t_steps[-1] = 0.0
+        x = noise * sigma_max # стартуем с N(0, sigma^2_T)
+        x_history = [self.normalize(noise)] # история для визуализации
+        with torch.no_grad():
+            for i in range(len(t_steps) - 1):
+                t_cur = t_steps[i]
+                t_next = t_steps[i + 1]
+                t_net = t_steps[i] * torch.ones(x.shape[0], device=device)
+                delta_t = (t_next - t_cur).abs()
+                x = x + self.velocity_from_denoiser(x, net, t_net, class_labels=labels, stochastic=stochastic) * delta_t
+                if stochastic:
+                    x = x + torch.sqrt(2 * delta_t * t_cur) * torch.randn_like(x)
+
+                x_history.append(self.normalize(x).view(-1, self.n_channels, *x.shape[2:]))
+
+        # добавляем начало, конец, и выкидываем часть траектории
+        x_history = [x_history[0]] + x_history[::-(num_steps // (vis_steps - 2))][::-1] + [x_history[-1]]
+        return x, x_history
+        
 
     def get_name(self):
         return "Euler"
