bc4f8a420566257fd3fadc402ad7d08d7249d232
Arseniy <arsenator337@gmail.com>
2024-12-20 16:35:05 +0300

    fix

--- models/solvers/base.py
+++ models/solvers/base.py
@@ -7,19 +7,16 @@ class BaseSolver(object):
     def __call__(self, net, *args, **kwds):
         raise NotImplementedError()
     
-    @abstractmethod
-    def normalize(x):
+    def normalize(self, x):
         return x / x.abs().max(dim=0)[0][None, ...]
     
-    @abstractmethod
-    def get_timesteps(sigma_min=0.02, sigma_max=80.0, num_steps=20, device='cuda', rho=7.0):
+    def get_timesteps(self, sigma_min=0.02, sigma_max=80.0, num_steps=20, device='cuda', rho=7.0):
         step_indices = torch.arange(num_steps, device=device)
         t_steps = (sigma_max ** (1 / rho) + step_indices / (num_steps - 1) * (sigma_min ** (1 / rho) - sigma_max ** (1 / rho))) ** rho
         t_steps = torch.cat([t_steps, torch.zeros_like(t_steps[:1])]) # t_N = 0
         return t_steps
     
-    @abstractmethod
-    def velocity_from_denoiser(x, net, sigma, class_labels=None, error_eps=1e-4, stochastic=False):
+    def velocity_from_denoiser(self, x, net, sigma, class_labels=None, error_eps=1e-4, stochastic=False):
         sigma = sigma[:, None, None, None] # [batch, 1, 1, 1]
         v = (net(x, sigma, class_labels) - x) / (sigma + error_eps)
 
--- models/solvers/euler.py
+++ models/solvers/euler.py
@@ -12,7 +12,7 @@ class EulerSolver(BaseSolver):
             sigma_max=80.0, num_steps=20, device='cuda', 
             rho=7.0, stochastic=False, vis_steps=5
     ):
-        t_steps = self.get_timesteps(sigma_min, sigma_max, num_steps, device, rho) # t_steps[0] = 80.0, t_steps[-2] = 0.02, t_steps[-1] = 0.0
+        t_steps = BaseSolver.get_timesteps(sigma_min, sigma_max, num_steps, device, rho) # t_steps[0] = 80.0, t_steps[-2] = 0.02, t_steps[-1] = 0.0
         x = noise * sigma_max # стартуем с N(0, sigma^2_T)
         x_history = [self.normalize(noise)] # история для визуализации
         with torch.no_grad():
